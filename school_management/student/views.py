b1f03d47c97a1f5c70e5af7e5333990ebe83111a
mdsamaul <mdsamaul843@gmail.com>
2026-02-09 15:43:21 +0600

    Add templates and update views for student and teacher dashboards

--- school_management/student/views.py
+++ school_management/student/views.py
@@ -1,8 +1,12 @@
 from urllib import request
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
+# def profile(request):
+#     return HttpResponse("profile page   ")
+
 def profile(request):
-    return HttpResponse("profile page   ")
+    return render(request, 'student/index.html')
--- school_management/teacher/urls.py
+++ school_management/teacher/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib import admin
-
+from . import views
 urlpatterns=[
     path('admin/', admin.site.urls),   
+    path('profile/', views.profile),
     
     ]
